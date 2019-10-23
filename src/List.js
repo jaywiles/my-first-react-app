ef98d3bdf78322c442a559f925035d4ab2f6c2e3
jaywiles <jay.wiles@gmail.com>
2019-10-22 20:52:24 -0500

    changes again

--- src/List.js
+++ src/List.js
@@ -4,8 +4,20 @@ const List = props => (
   <ul>
     {
       props.items.map((item, index) => (
-            <li key={index}>{item}<button>EDIT</button>
-                <input value={item}></input>
+            // <li key={index}>{item}<button>EDIT</button>
+            //     <input value={item}></input>
+            // </li>
+
+            <li key={index}>
+                {isEditing ? (
+                    <div>
+                        <input value={item}></input>
+                    </div>
+                ):(
+                    <div>
+                        <span>{item}</span><button>EDIT</button>
+                    </div>
+                )}
             </li>
         )
       )
